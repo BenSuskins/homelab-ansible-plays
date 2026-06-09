5618cfda0dcb80ba6659307d12b02c0e2a27e328
Ben Suskins <suskinsdevelopment@gmail.com>
2026-06-09 21:41:17 +0100

    Create

--- terraform/proxmox/docker.tf
+++ terraform/proxmox/docker.tf
@@ -1,21 +1,21 @@
-# module "docker" {
-#   source            = "./modules/vm"
-#   name              = "docker"
-#   vm_id             = 202
-#   node_name         = var.proxmox_node
-#   clone_template_id = 9000
-#   cores             = 2
-#   memory            = 2048
-#   disk_size         = 64
-#   ip_address        = "192.168.0.202/24"
-#   ssh_public_keys   = [file("~/.ssh/homelab.pub")]
-#   description       = "Docker Host"
-#   bios              = "ovmf"
-#   startup_order     = 2
-#   started           = false
-# }
+module "docker" {
+  source            = "./modules/vm"
+  name              = "docker"
+  vm_id             = 202
+  node_name         = var.proxmox_node
+  clone_template_id = 9000
+  cores             = 2
+  memory            = 2048
+  disk_size         = 64
+  ip_address        = "192.168.0.202/24"
+  ssh_public_keys   = [file("~/.ssh/homelab.pub")]
+  description       = "Docker Host"
+  bios              = "ovmf"
+  startup_order     = 2
+  started           = false
+}
 
-# output "docker_ipv4" {
-#   description = "IPv4 addresses reported by the docker VM guest agent"
-#   value       = module.docker.ipv4_addresses
-# }
+output "docker_ipv4" {
+  description = "IPv4 addresses reported by the docker VM guest agent"
+  value       = module.docker.ipv4_addresses
+}
--- terraform/proxmox/mediaserver.tf
+++ terraform/proxmox/mediaserver.tf
@@ -1,25 +1,25 @@
-# module "mediaserver" {
-#   source            = "./modules/vm"
-#   name              = "mediaserver"
-#   vm_id             = 201
-#   node_name         = var.proxmox_node
-#   clone_template_id = 9000
-#   cores             = 2
-#   memory            = 2048
-#   disk_size         = 64
-#   ip_address        = "192.168.0.201/24"
-#   ssh_public_keys   = [file("~/.ssh/homelab.pub")]
-#   description       = "Media Server"
-#   bios              = "ovmf"
-#   started = false
+module "mediaserver" {
+  source            = "./modules/vm"
+  name              = "mediaserver"
+  vm_id             = 201
+  node_name         = var.proxmox_node
+  clone_template_id = 9000
+  cores             = 2
+  memory            = 2048
+  disk_size         = 64
+  ip_address        = "192.168.0.201/24"
+  ssh_public_keys   = [file("~/.ssh/homelab.pub")]
+  description       = "Media Server"
+  bios              = "ovmf"
+  started = false
 
-#   # Boots last so the NAS has time to come up first; the 240s gap is set as
-#   # development's up_delay (the preceding guest), since Proxmox up_delay
-#   # applies before the NEXT guest starts.
-#   startup_order = 5
-# }
+  # Boots last so the NAS has time to come up first; the 240s gap is set as
+  # development's up_delay (the preceding guest), since Proxmox up_delay
+  # applies before the NEXT guest starts.
+  startup_order = 5
+}
 
-# output "mediaserver_ipv4" {
-#   description = "IPv4 addresses reported by the mediaserver VM guest agent"
-#   value       = module.mediaserver.ipv4_addresses
-# }
+output "mediaserver_ipv4" {
+  description = "IPv4 addresses reported by the mediaserver VM guest agent"
+  value       = module.mediaserver.ipv4_addresses
+}
